632b202a83ca36bc21f494eec7885fd5413d32b5
Kskskfi <ogyrhik321@gmail.com>
2025-02-07 10:39:38 +0700

    methodOnSubmit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,56 @@
 let product = "Socks";
 
 
+Vue.component('product-review', {
+    template: `
+    <form class="review-form" @submit.prevent="onSubmit">
+    <p>
+    <label for="name">Name:</label>
+    <input id="name" v-model="name" placeholder="name">
+    </p>
 
+    <p>
+    <label for="review">Review:</label>
+    <textarea id="review" v-model="review"></textarea>
+    </p>
+
+    <p>
+    <label for="rating">Rating:</label>
+    <select id="rating" v-model.number="rating">
+        <option>5</option>
+        <option>4</option>
+        <option>3</option>
+        <option>2</option>
+        <option>1</option>
+    </select>
+    </p>
+
+    <p>
+    <input type="submit" value="Submit"> 
+    </p>
+    </form>
+  `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null
+        }
+    },
+    methods:{
+        onSubmit() {
+            let productReview = {
+                name: this.name,
+                review: this.review,
+                rating: this.rating
+            }
+            this.name = null
+            this.review = null
+            this.rating = null
+        }
+    }
+ })
+ 
 
 Vue.component('product', {
     props: {
